51ae0cd90bfc31e74b538d57c0445f57635d3522
zhanggn <answer.cg@gmail.com>
2013-08-30 16:29:37 +0800

    1

---
+++ generated/cn/zhanggn/zcms/domain/NewTest.java
@@ -0,0 +1,13 @@
+package cn.zhanggn.zcms.domain;
+
+/**
+ * User: zhanggn
+ * Date: 13-8-30
+ * Time: 下午4:23
+ */
+public class NewTest {
+
+    public static void main(String[] args) {
+        System.out.println("asdf");
+    }
+}
